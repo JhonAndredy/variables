c97240d5acc30f6253a4a8b479a17915b2e5ba90
JhonAndredy <jhonandredi8705@gmail.com>
2022-10-27 08:45:23 -0500

    first commit

---
+++ varible.php
@@ -0,0 +1,16 @@
+<?php
+
+    //declaracion de variables
+    $numero = 5;
+    $num2 = 2;
+    $suma = $numero + $num2;
+    /* $cadena = "Yo Soy Una Cadena De Texto"; */
+    
+    echo $numero + $num2;
+    echo "<br>";
+    echo $suma;
+    /* echo $cadena;
+    echo "<br>";
+    echo $numero." ".$cadena; */
+
+?>
